947e6081fbaf19e550280d142ab0a16a3e309482
Budur <balamri0087@stu.kau.edu.sa>
2022-08-08 16:02:09 +0300

    delete comment

--- EstashirEbtakir/SignUp.aspx.designer.cs
+++ EstashirEbtakir/SignUp.aspx.designer.cs
@@ -69,15 +69,6 @@ namespace EstashirEbtakir
         protected global::System.Web.UI.HtmlControls.HtmlInputPassword Password2;
 
         /// <summary>
-        /// uniID control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText uniID;
-
-        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
