c4be44bd5cab9491f3dae9ce60413ea06137b9df
Renad Al-Qarni <90710515+RenadQarni@users.noreply.github.com>
2022-08-09 11:28:49 +0300

    Admin profile back-end

--- EstashirEbtakir/AdminProfile.aspx.cs
+++ EstashirEbtakir/AdminProfile.aspx.cs
@@ -15,10 +15,25 @@ namespace EstashirEbtakir {
         SqlConnection con;
         protected void Page_Load(object sender, EventArgs e) {
 
+            // set database connection
             con = new SqlConnection(getConstring());
             con.Open();
 
+            // get the session id ( admin id )
+            int sessionID = int.Parse(HttpContext.Current.Session.SessionID);
 
+            // get admin info from DB
+            SqlCommand cmd = new SqlCommand("select * from OurAdmin where Admin_ID='" + sessionID + "'", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read()) {
+                // display admin info
+                Fname.Attributes.Add("placeholder", reader.GetString(1));
+                Lname.Attributes.Add("placeholder", reader.GetString(1) + " " + reader.GetString(2));
+                name.Attributes.Add("placeholder", reader.GetString(2));
+                email.Attributes.Add("placeholder", reader.GetString(3));
+            }
+            con.Close();
         }
 
         public string getConstring() {
