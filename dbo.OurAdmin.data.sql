fcb3e2d91de012a4be94c74314892e002f189101
Budur <balamri0087@stu.kau.edu.sa>
2022-08-10 08:53:52 +0300

    database

---
+++ dbo.OurAdmin.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[OurAdmin] ([Admin_ID], [Fname], [Lname], [Password], [Email]) VALUES (155, N'هند', N'محمد', N'H#123123h', N'renadalgarni0@gmail.com')
+INSERT INTO [dbo].[OurAdmin] ([Admin_ID], [Fname], [Lname], [Password], [Email]) VALUES (199, N'أمل', N'أحمد', N'A#123123a', N'raanoodiz@gmail.com')
