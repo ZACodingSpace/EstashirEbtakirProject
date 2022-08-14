686ebcfc50ecf83a8c639ab75779e33ae7ff7b62
Budur <balamri0087@stu.kau.edu.sa>
2022-08-14 20:04:49 +0300

    add idea c#

--- EstashirEbtakir/AddIdea.aspx.cs
+++ EstashirEbtakir/AddIdea.aspx.cs
@@ -21,11 +21,7 @@ namespace EstashirEbtakir
         //int ID;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                Session.Remove("error");
-                Session["id"] = null;
-            }
+
         }
         public string getConstring()
         {
@@ -34,6 +30,7 @@ namespace EstashirEbtakir
         }
         protected void add_Click(object sender, EventArgs e)
         {
+            //MessageBox.Show(Session["id"] + " ");
             string ideaName = ideaNameField.Value;
             string brief = ideaDescriptionTextarea.Value;
 
@@ -41,13 +38,18 @@ namespace EstashirEbtakir
             con = new SqlConnection(str);
             con.Open();
             SqlCommand cmd = new SqlCommand("INSERT INTO OurIdea(User_ID, Idea_Name, Brief,Idea_State) VALUES(@userID,@IdeaName, @IdeaBrief, @State)", con);
-            cmd.Parameters.AddWithValue("@userID", Session["id"]);
+            int UserID = (int)Session["id"];
+            cmd.Parameters.AddWithValue("@userID", UserID);
             cmd.Parameters.AddWithValue("@IdeaName", ideaName);
-            cmd.Parameters.AddWithValue("@Brief", brief);
+            cmd.Parameters.AddWithValue("@IdeaBrief", brief);
             cmd.Parameters.AddWithValue("@State", 0);
             cmd.ExecuteNonQuery();
-            //SqlCommand cmd2 = new SqlCommand("SELECT IDENT_CURRENT(OurIdea)", con);
-            //ID = cmd2.ExecuteReader();
+            done.Text = "تمت إضافة الفكرة بنجاح";
+            SqlCommand cmd2 = new SqlCommand("select Idea_ID from OurUser where User_ID='" + UserID + ", Idea_Name='" + ideaName + "'", con);
+
+           // SqlDataReader reader= cmd2.ExecuteReader();
+            //int lastID = reader.GetDecimal(Idea_ID);
+            //MessageBox.Show(lastID+" ");
             SqlCommand cmdTech = new SqlCommand("INSERT INTO Technologies(tech_name, type, ID) VALUES(@TechName,@Type, @Idea_ID)", con);
            
             if (VRtech.Checked == true)
