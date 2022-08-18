8ad36efa132b32ceebb3e9f6d7f06c9db768af00
Sara <sara757520@gmail.com>
2022-08-18 11:04:29 +0300

    .

--- EstashirEbtakir/StatisticsPage.aspx.cs
+++ EstashirEbtakir/StatisticsPage.aspx.cs
@@ -29,13 +29,30 @@ namespace EstashirEbtakir
             //Chart1.ChartAreas[0].AxisX.LabelStyle.Font = new System.Drawing.Font("Arial", 14f);
             //Chart1.AntiAliasing = AntiAliasingStyles.All;
 
-            ctl03.Palette = ChartColorPalette.None;
-            ctl03.PaletteCustomColors = new Color[] {
+            Chart2.Palette = ChartColorPalette.None;
+            Chart2.PaletteCustomColors = new Color[] {
                 ColorTranslator.FromHtml("#377D71"),
                 ColorTranslator.FromHtml("#FBC5C5"),
                 ColorTranslator.FromHtml("#FBA1A1"),
                 ColorTranslator.FromHtml("#8879B0")
             };
+
+            Chart3.Palette = ChartColorPalette.None;
+            Chart3.PaletteCustomColors = new Color[] {
+                ColorTranslator.FromHtml("#FBA1A1"),
+                ColorTranslator.FromHtml("#FBC5C5"),
+                ColorTranslator.FromHtml("#377D71"),
+                ColorTranslator.FromHtml("#8879B0")
+            };
+
+            Chart4.Palette = ChartColorPalette.None;
+            Chart4.PaletteCustomColors = new Color[] {
+                ColorTranslator.FromHtml("#8879B0"),
+                ColorTranslator.FromHtml("#FBC5C5"),
+                ColorTranslator.FromHtml("#377D71"),
+                ColorTranslator.FromHtml("#FBA1A1")
+            };
+
         }
 
     }
