4c10343fc393be2aeb84177753fe181b37504c79
Budur <balamri0087@stu.kau.edu.sa>
2022-08-23 18:43:57 +0300

    event

--- EstashirEbtakir/UserViewEvent.aspx.cs
+++ EstashirEbtakir/UserViewEvent.aspx.cs
@@ -4,14 +4,75 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace EstashirEbtakir
 {
 	public partial class UserViewEvent : System.Web.UI.Page
 	{
+		SqlConnection sqlconn;
+		int FileID = 0;
 		protected void Page_Load(object sender, EventArgs e)
 		{
+            var Id = Request.QueryString["id"];
+            string mainconn = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
 
-		}
-	}
+            string sqlquery = "Select * from [dbo].[EventsAndParticipation] where ID='" + Id + "'";
+            SqlDataAdapter da = new SqlDataAdapter(sqlquery, sqlconn);
+            DataTable ds = new DataTable();
+            da.Fill(ds);
+            eventName.InnerText = (string)ds.Rows[0].ItemArray[2];
+            eventdes.InnerText = (string)ds.Rows[0].ItemArray[3];
+            supervisorP.Value = (string)ds.Rows[0].ItemArray[4];
+            FileID = (int)ds.Rows[0].ItemArray[8];
+            byte[] bytes = (byte[])ds.Rows[0].ItemArray[6];
+            string base64String = Convert.ToBase64String(bytes);
+            Image1.ImageUrl = "data:image/png;base64," + base64String;
+
+            sqlconn.Close();
+
+        }
+        protected void downloadFile_Click(object sender, EventArgs e)
+        {
+            int id = FileID;
+            byte[] bytes;
+            string fileName, contentType;
+            string constr = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "select Name, Data, ContentType from EEFile where Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        bytes = (byte[])sdr["Data"];
+                        contentType = sdr["ContentType"].ToString();
+                        fileName = sdr["Name"].ToString();
+                    }
+                    con.Close();
+                }
+            }
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = contentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+            Response.BinaryWrite(bytes);
+            Response.Flush();
+            Response.End();
+        }
+
+
+    }
 }
---
+++ dbo.EEIdea.data.sql
@@ -0,0 +1,13 @@
+﻿SET IDENTITY_INSERT [dbo].[EEIdea] ON
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (1, 4, NULL, N'القياس الذكي', N'2022-08-22', N'تطبيق يقيس الملابس بالمحلات التجاريه على الاشخاص بدلا من قياسها ويعتمد على ادخال البيانات ويكون بوضع صوره للشخص ورؤيه الملابس ومناسبتها وقياسها عليه بطريقه افتراضيه', 1, 0, NULL)
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (2, 4, NULL, N'القياس الذكي', N'2022-08-22', N'????? ???? ??????? ???????? ???????? ??? ??????? ???? ?? ?????? ?????? ??? ????? ???????? ????? ???? ???? ????? ????? ??????? ????????? ??????? ???? ?????? ????????', 0, 0, NULL)
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (3, 5, NULL, N'التحكم بالأجهزة الكهربائية', N'2022-08-22', N'تطبيق التحكم عن بعد في اجهزة كهربائية 
+طلاب الهندسة', 1, 0, NULL)
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (4, 6, NULL, N'تسويق ', N'2022-08-22', N'فكرة تقديم منتجات جديدة في التسويق عن طريق اشتراك العامة في تحديد شكل المنتج 
+فكرة في مجال التعليم تصميم محتوى رقمي لمادة علمية كالتسويق الإلكتروني', -1, 0, NULL)
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (5, 7, NULL, N'سجل الأسرة الرقمي', N'2022-08-22', N'سجل أسرة الأم الرقمي وفق رؤية 2030 للإثبات صلة القرابة بين الأمهات والأولاد', -1, 0, NULL)
+INSERT INTO [dbo].[EEIdea] ([Idea_ID], [User_ID], [Admin_ID], [Idea_Name], [Date], [Brief], [Idea_State], [Is_Taken], [Taker_ID]) VALUES (6, 8, NULL, N'مولد الخرائط', N'2022-08-22', N'عمل الخرائط الذهنية الالكترونية لمناهج اللغة العربية من الابتدائي إلى الثانوي', 1, 0, NULL)
+SET IDENTITY_INSERT [dbo].[EEIdea] OFF
+
+
+UPDATE EEIdea SET Brief= N'تطبيق يقيس الملابس بالمحلات التجاريه على الاشخاص بدلا من قياسها ويعتمد على ادخال البيانات ويكون بوضع صوره للشخص ورؤيه الملابس ومناسبتها وقياسها عليه بطريقه افتراضيه' WHERE Idea_ID= 1;
