4118245f2bcf1d790c6552aff81d0fd6fc0cf5be
Budur <balamri0087@stu.kau.edu.sa>
2022-08-23 10:47:19 +0300

    الكثير والكثير

--- EstashirEbtakir/AcceptIdeas.aspx.cs
+++ EstashirEbtakir/AcceptIdeas.aspx.cs
@@ -24,19 +24,61 @@ namespace EstashirEbtakir
             
             string sqlqueryIdea = "Select * from [dbo].[EEIdea] where Idea_ID=" + Id;
             SqlCommand sqlcommIdea = new SqlCommand(sqlqueryIdea, sqlconn);
-            sqlconn.Open();
-            SqlDataAdapter sdaIdea = new SqlDataAdapter();
-            sdaIdea.SelectCommand = sqlcommIdea;
-            DataSet dsIdea = new DataSet();
+            sqlconn.Open(); 
+            string sqlquery = "Select * from [dbo].[EEIdea] where Idea_Id='" + Id + "'";
+            SqlDataAdapter da = new SqlDataAdapter(sqlquery, sqlconn);
+            DataTable ds = new DataTable();
+            da.Fill(ds);
+            int ideaID = (int)ds.Rows[0].ItemArray[0];
+            ideaDetailsNameField.Value = (string)ds.Rows[0].ItemArray[3];
+            ideaDetailsDescription.Value = (string)ds.Rows[0].ItemArray[5];
 
-            sdaIdea.Fill(dsIdea);
-            GridViewAcceptIdeas.DataSource = dsIdea;
-            GridViewAcceptIdeas.DataBind();
+            string sqlquery2 = "Select * from [dbo].[Technologies] where ID='" + ideaID + "' And type= 'I'";
+            SqlDataAdapter da2 = new SqlDataAdapter(sqlquery2, sqlconn);
+            DataTable ds2 = new DataTable();
+            da2.Fill(ds2);
+
+            int i = 0;
+            while (ds2.Rows.Count > i)
+            {
+                Text3.Value += ds2.Rows[i].ItemArray[0] + ", ";
+                i = i + 1;
+            }
+
             sqlconn.Close();
 
         }
 
+        protected void AcceptB_Click(object sender, EventArgs e)
+        {
+            var Id = Request.QueryString["id"];
+
+            string mainconn = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE EEIdea SET Idea_State= 1 WHERE Idea_ID='" +Id + "'", sqlconn);
+            cmd.ExecuteNonQuery();
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert",
+                                    "Swal.fire({title: 'تم قبول الفكرة بنجاح',icon: 'success', confirmButtonText: 'موافق'}).then(function() { window.location = 'AcceptIdeaList.aspx'})", true);
+
+        }
+
+        protected void RejectB_Click(object sender, EventArgs e)
+        {
+            var Id = Request.QueryString["id"];
+
+            string mainconn = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+
+            SqlCommand cmd = new SqlCommand("UPDATE EEIdea SET Idea_State= -1 WHERE Idea_ID='" + Id + "'", sqlconn);
+            cmd.ExecuteNonQuery();
+
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert",
+                                    "Swal.fire({title: 'تم رفض الفكرة بنجاح',icon: 'success', confirmButtonText: 'موافق'}).then(function() { window.location = 'AcceptIdeaList.aspx'})", true);
 
 
+        }
     }
 }
--- EstashirEbtakir/ProjectDetails.aspx.cs
+++ EstashirEbtakir/ProjectDetails.aspx.cs
@@ -19,17 +19,32 @@ namespace EstashirEbtakir
             var Id = Request.QueryString["id"];
             string mainconn = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
              sqlconn = new SqlConnection(mainconn);
+             sqlconn.Open();
 
-                sqlconn.Open();
-
-            string sqlquery = "Select * from [dbo].[EEProject] where Project_Id=" + Id;
-            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
-
-            SqlDataAdapter da = new SqlDataAdapter();
-            da.SelectCommand = sqlcomm;
-            DataSet ds = new DataSet();
+            string sqlquery = "Select * from [dbo].[EEProject] where Project_Id='" + Id + "'";
+            SqlDataAdapter da = new SqlDataAdapter(sqlquery, sqlconn);
+            DataTable ds = new DataTable();
             da.Fill(ds);
+            int ProID = (int)ds.Rows[0].ItemArray[0];
+            projectDetailsNameField.Value= (string)ds.Rows[0].ItemArray[3];
+            ProjectDetailsDescription.Value = (string)ds.Rows[0].ItemArray[6];
+            collegeProjectDetails.Value= (string)ds.Rows[0].ItemArray[7];
+            majorProjectDetails.Value= (string)ds.Rows[0].ItemArray[8];
+            gradeP.Value= ds.Rows[0].ItemArray[9]+" ";
+            TextareaProjectDetails.Value = (string)ds.Rows[0].ItemArray[5];
+            supervisorP.Value = (string)ds.Rows[0].ItemArray[4];
 
+            string sqlquery2 = "Select * from [dbo].[Technologies] where ID='" + ProID + "' And type= 'P'";
+            SqlDataAdapter da2 = new SqlDataAdapter(sqlquery2, sqlconn);
+            DataTable ds2 = new DataTable();
+            da2.Fill(ds2);
+
+            int i = 0;
+            while (ds2.Rows.Count > i)
+            {
+                Text1.Value += ds2.Rows[i].ItemArray[0] + ", ";
+                i = i + 1;
+            }
 
             sqlconn.Close();
            
